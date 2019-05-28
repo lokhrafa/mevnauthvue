e1562318aadf457b247a9a8d2d2ae045ceb3363d
lokhrafa <lokhrafael@hotmail.com>
2019-05-28 12:52:49 -0400

    Production Database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,14 @@
-module.exports = {
+if(process.env.NODE_ENV === 'production')
+{
+  module.exports ={
+   mongoURI: "mongodb://lokhrafa:rafael123@ds131905.mlab.com:31905/mevnauthdatabase",
+   secret: 'yoursecret'
+  }  
+
+} else {
+    module.exports = {
         mongoURI: "mongodb://localhost:27017/meven_auth",
         secret: 'yoursecret'
     }
+
+}
